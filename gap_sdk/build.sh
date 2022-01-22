4e6dd8e3b845a6d028e77050ef4b7aa713414cdf
Colin <csamplawski@cs.umass.edu>
2022-01-21 20:03:01 -0500

    initial gap_sdk commit

---
+++ gap_sdk/build.sh
@@ -0,0 +1,2 @@
+#sudo docker build -t gap:base -f base.Dockerfile .
+sudo docker build -t gap_sdk -f gap_sdk.Dockerfile .
---
+++ gap_sdk/run.sh
@@ -0,0 +1,2 @@
+#sudo docker run -i --rm -t gap_sdk bash -c 'source /gap_sdk/configs/gapuino_v2.sh'
+sudo docker run -i --rm -t gap_sdk bash
