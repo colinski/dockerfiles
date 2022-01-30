bc17bf4285c9c5ab39c4454d1f25e3549efa8d95
Colin <csamplawski@cs.umass.edu>
2022-01-30 16:18:05 -0500

    add mmcv container

---
+++ mmcv/build.sh
@@ -0,0 +1,1 @@
+sudo docker build -t mmcv:latest -f Dockerfile .
---
+++ mmcv/run.sh
@@ -0,0 +1,2 @@
+sudo docker run -it --rm -p 8080:8888 mmcv jupyter lab --port 8888 --no-browser --allow-root --ip 0.0.0.0
+#sudo docker run -it --rm mmcv:latest bash
