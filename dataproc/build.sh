a5458dabc9937e522f487fdf704c066936038565
Colin <csamplawski@cs.umass.edu>
2023-12-05 10:42:36 -0500

    added dataproc container

---
+++ dataproc/build.sh
@@ -0,0 +1,2 @@
+#DOCKER_BUILDKIT=0 sudo docker build --no-cache -t colinski/python310_cuda118 -f Dockerfile .
+sudo docker build -t colinski/dataproc -f Dockerfile .
---
+++ dataproc/run.sh
@@ -0,0 +1,2 @@
+sudo docker run -it --rm --gpus all colinski/dataproc bash
+#sudo docker run -it --rm mmcv:latest bash
